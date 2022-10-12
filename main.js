e47690076b06ff61204bf024654e4310649e7d48
luca-scocchi <lscocchi@gmail.com>
2022-10-12 17:23:06 +0200

    main.js changed

--- main.js
+++ main.js
@@ -1,21 +1,57 @@
-const counter = document.querySelector("#counter");
+
+
+
+let container = document.querySelector(".container");
+let counter = document.createElement("div");
+counter.id = "counter" ;
+counter.innerHTML = "0";
+counter.classList.add("counter");
+container.appendChild(counter);
+
+let btnContainer = document.createElement("div");
+container.appendChild(btnContainer);
+let btnDecrease = document.createElement("button");
+btnDecrease.id = "decrease" ;
+btnDecrease.classList.add("btn");
+btnContainer.appendChild(btnDecrease);
+btnDecrease.innerHTML = "-" ;
+
+let btnReset = document.createElement("button");
+btnReset.id = "reset" ;
+btnReset.classList.add("btn");
+btnContainer.appendChild(btnReset);
+btnReset.innerHTML = "reset" ;
+
+let btnIncrease = document.createElement("button");
+btnIncrease.id = "increase" ;
+btnIncrease.classList.add("btn");
+btnContainer.appendChild(btnIncrease);
+btnIncrease.innerHTML = "+" ;
+
+
+
+
+
+
+
+
+
 const btns = document.querySelectorAll(".btn");
 
-// initialize the count variable
 let count = 0;
 
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    const styles = e.currentTarget.classList;
+    const id = e.currentTarget.id;
 
-    if (styles.contains("increase")) {
+    if (id.includes("increase")) {
       count++;
       counter.classList.add("up");
 
       setTimeout(() => {
         counter.classList.remove("up");
       }, 500);
-    } else if (styles.contains("decrease")) {
+    } else if (id.includes("decrease")) {
       count--;
       counter.classList.add("down");
 
